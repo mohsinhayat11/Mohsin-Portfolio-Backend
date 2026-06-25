f1ffacfebd0d0a823a9eec06aa273c2c7da290e3
Mohsin Hayat <mohsinmalak745@gmail.com>
2026-06-25 12:10:09 -0700

    Fix Nodemailer SMTP configuration

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -8,7 +8,9 @@ const createContact = async (req, res) => {
 
     // Email Transporter
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
